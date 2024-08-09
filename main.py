f6c2f54aefd8b541c276a553078a6655285b1b81
Lahiru Gunathilake <gunathilakebllg@gmail.com>
2024-08-09 20:13:59 +1000

    Test file completed

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+# Imports
+from analyzeByHeight import analyze_by_height
+from analyzeByColor import analyze_by_color
+from Height.heightData import generate_height_data
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import cv2
 
 
-# Press the green button in the gutter to run the script.
+def image_analyze(analyze_function, surface, lower_mid=0, upper_mid=0):
+    if analyze_function == "color":
+        print(analyze_by_color(surface))
+    else:
+        print(analyze_by_height(surface, lower_mid, upper_mid))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    function_color = "color"  # "color" or "height"
+    function_height = "height"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # COLOR DATA
+    # Test case 1
+    image_multicolor = cv2.imread('Images/sample2.jpg')
+    image_rgb = cv2.cvtColor(image_multicolor, cv2.COLOR_BGR2RGB)
+    python_2d_rgb_array = image_rgb.tolist()
+    image_analyze(function_color, python_2d_rgb_array)
+
+    # Test case 2
+    image_red = cv2.imread('Images/red.jpg')
+    image_rgb = cv2.cvtColor(image_red, cv2.COLOR_BGR2RGB)
+    python_2d_rgb_array = image_rgb.tolist()
+    image_analyze(function_color, python_2d_rgb_array)
+
+    # Test case 3
+    image_green = cv2.imread('Images/green.jpg')
+    image_rgb = cv2.cvtColor(image_green, cv2.COLOR_BGR2RGB)
+    python_2d_rgb_array = image_rgb.tolist()
+    image_analyze(function_color, python_2d_rgb_array)
+
+    # Test case 4
+    image_blue = cv2.imread('Images/blue.jpg')
+    image_rgb = cv2.cvtColor(image_blue, cv2.COLOR_BGR2RGB)
+    python_2d_rgb_array = image_rgb.tolist()
+    image_analyze(function_color, python_2d_rgb_array)
+
+    # HEIGHT DATA
+    # Test case 5
+    surface_height_data = generate_height_data(100, 100, 0, 100)
+    image_analyze(function_height, surface_height_data, 40, 60)
