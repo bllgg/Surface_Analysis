6e27f010a5af4bb9755c46b28d7fc18baa51ffa8
Lahiru Gunathilake <gunathilakebllg@gmail.com>
2024-08-09 19:23:02 +1000

    Analyze by height function done

---
+++ analyzeByHeight.py
@@ -0,0 +1,30 @@
+"""
+Surface is a 2D array which contains surface height data
+[
+[h11, h12, h13, ... , h1m],
+[h21, h22, h23, ... , h2m],
+.
+.
+.
+[hn1, hn2, hn3, ... , hnm]
+]
+"""
+
+
+def analyze_by_height(surface, lower_mid_val, upper_mid_val):
+    low_height_area = 0.0
+    mid_height_area = 0.0
+    high_height_area = 0.0
+    for i in range(len(surface)):
+        for j in range(len(surface[i])):
+            if surface[i][j] > upper_mid_val:
+                high_height_area += 1.0
+            elif surface[i][j] > lower_mid_val:
+                mid_height_area += 1.0
+            else:
+                low_height_area += 1.0
+    sum_of_areas = low_height_area + mid_height_area + high_height_area
+    low_height_proportion = low_height_area / sum_of_areas
+    mid_height_proportion = mid_height_area / sum_of_areas
+    high_height_proportion = high_height_area / sum_of_areas
+    return low_height_proportion, mid_height_proportion, high_height_proportion
